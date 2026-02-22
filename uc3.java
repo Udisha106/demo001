a90de6e96fbbd45f73790b8cd1d094d9e5d68d1c
Udisha Srivastava <udishasrivastava106@gamil.com>
2026-02-22 19:47:32 +0530

    Added uc3

--- uc3.java
+++ uc3.java
@@ -1,12 +1,12 @@
+import java.util.*;
 public class SumTwoNumbers {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the first number:");
         int num1 = scanner.nextInt();
         System.out.println("Enter the second number:");
         int num2 = scanner.nextInt();
         int sum = num1 + num2;
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
-        scanner.close();
     }
 }
