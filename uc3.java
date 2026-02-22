e3b5c5ef0245e7add03329f3ff46a7914b473a25
Udisha106 <udishasrivastava106@gmail.com>
2026-02-22 19:49:39 +0530

    Update uc3.java

--- uc3.java
+++ uc3.java
@@ -1,12 +1,13 @@
-import java.util.*;
-public class SumTwoNumbers {
+public class UC3 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the first number:");
-        int num1 = scanner.nextInt();
-        System.out.println("Enter the second number:");
-        int num2 = scanner.nextInt();
-        int sum = num1 + num2;
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
-    }
+       
+       System.out.println(String.join(" " , "   ***   " , "*******" , "*******" , "*******"));
+	   System.out.println(String.join(" " , " **   ** " , "**   **" , "**   **" , "**     "));
+	   System.out.println(String.join(" " , "**     **" , "**   **" , "**   **" , "**     "));
+	   System.out.println(String.join(" " , "**     **" , "*******" , "*******" , "*******"));
+	   System.out.println(String.join(" " , "**     **" , "**     " , "**     " , "     **"));
+	   System.out.println(String.join(" " , " **   ** " , "**     " , "**     " , "     **"));
+	   System.out.println(String.join(" " , "   ***   " , "**     " , "**     " , "*******"));
+	   
+	   }
 }
